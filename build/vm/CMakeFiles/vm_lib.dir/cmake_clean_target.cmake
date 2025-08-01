f4fa7ba03093caedcc3fbdbce0128a961f2860a2
venkatpvc25 <venkatpvc25@gmail.com>
2025-08-01 12:50:55 +0530

    removed build folder

--- build/vm/CMakeFiles/vm_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libvm_lib.a"
-)
--- build/vm/CMakeFiles/vm_lib.dir/src/assembler.c.o.d
+++
@@ -1,3 +0,0 @@
-vm/CMakeFiles/vm_lib.dir/src/assembler.c.o: \
- /home/venkat/git/c-projects/Virtual\ Machine/vm/src/assembler.c \
- /usr/include/stdc-predef.h
